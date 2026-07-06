4faef572ba59f82e2894c879ff6c0e638469e677
shravan12092005 <shravanmole9383@gmail.com>
2026-07-06 23:19:56 +0530

    revert: page layout back to centered layout

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ from src.components.dialog_auto_enroll import auto_enroll_dialog
 def main():
     st.set_page_config(
         page_title='SnapClass - Making Attendance faster using AI',
-        page_icon="https://i.ibb.co/YTYGn5qV/logo.png",
-        layout="wide"
+        page_icon="https://i.ibb.co/YTYGn5qV/logo.png"
     )
 
     if 'login_type' not in st.session_state:
